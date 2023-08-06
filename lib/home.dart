b2468ff7e3933eda8513c6bcca4f0f8c47a56971
Shah Awais Bukhari <120441699+shahawais5@users.noreply.github.com>
2023-08-06 23:34:06 +0500

    Add files via upload

---
+++ lib/home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Shah Awais',style: TextStyle(fontSize: 14.sp),),
+            Text('Shah Awais',style: TextStyle(fontSize: 14.sp),)
+          ],
+        ),
+      ),
+    );
+  }
+}
